d3fe5269aa949901990415f9e559e36a6d496157
Alejandro Ramirez <alejandro@cs.txstate.edu>
2014-04-27 19:59:58 -0500

    Folder get/create tests

---
+++ tests/exchange2010/test_create_folder.py
@@ -0,0 +1,132 @@
+"""
+(c) 2013 LinkedIn Corp. All rights reserved.
+Licensed under the Apache License, Version 2.0 (the "License");?you may not use this file except in compliance with the License. You may obtain a copy of the License at  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software?distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+"""
+from httpretty import HTTPretty, httprettified
+from nose.tools import eq_, raises
+from pyexchange import Exchange2010Service
+
+from pyexchange.connection import ExchangeNTLMAuthConnection
+from pyexchange.exceptions import *
+
+from .fixtures import *
+
+
+class Test_PopulatingANewFolder():
+  """ Tests all the attribute setting works when creating a new event """
+  folder = None
+
+  @classmethod
+  def setUpAll(cls):
+
+    cls.folder = Exchange2010Service(
+      connection=ExchangeNTLMAuthConnection(
+        url=FAKE_EXCHANGE_URL,
+        username=FAKE_EXCHANGE_USERNAME,
+        password=FAKE_EXCHANGE_PASSWORD
+      )
+    ).folder()
+
+  def test_canary(self):
+    folder = self.folder.new_folder()
+    assert folder is not None
+
+  def test_folders_created_dont_have_an_id(self):
+    folder = self.folder.new_folder()
+    eq_(folder.id, None)
+
+  def test_folder_has_display_name(self):
+    folder = self.folder.new_folder(display_name=u'Conference Room')
+    eq_(folder.display_name, u'Conference Room')
+
+  def test_folder_has_default_folder_type(self):
+    folder = self.folder.new_folder()
+    eq_(folder.folder_type, u'Folder')
+
+  def test_folder_has_calendar_folder_type(self):
+    folder = self.folder.new_folder(folder_type=u'CalendarFolder')
+    eq_(folder.folder_type, u'CalendarFolder')
+
+
+class Test_CreatingANewFolder(object):
+  service = None
+  folder = None
+
+  @classmethod
+  def setUpAll(cls):
+    cls.service = Exchange2010Service(
+      connection=ExchangeNTLMAuthConnection(
+        url=FAKE_EXCHANGE_URL,
+        username=FAKE_EXCHANGE_USERNAME,
+        password=FAKE_EXCHANGE_PASSWORD,
+      )
+    )
+
+  def setUp(self):
+    self.folder = self.service.folder().new_folder()
+
+  @raises(AttributeError)
+  def test_folders_must_have_a_display_name(self):
+    self.parent_id = u'AQASAGFyMTY2AUB0eHN0YXRlLmVkdQAuAAADXToP9jZJ50ix6mBloAoUtQEAIXy9HV1hQUKHHMQm+PlY6QINNPfbUQAAAA=='
+    self.folder.create()
+
+  @raises(ValueError)
+  def test_events_must_have_a_parent_id(self):
+    self.folder.display_name = u'Conference Room'
+    self.parent_id = None
+    self.folder.create()
+
+  @raises(TypeError)
+  def cant_delete_an_uncreated_folder(self):
+    self.folder.delete()
+
+  @httprettified
+  def test_can_set_display_name(self):
+
+    HTTPretty.register_uri(
+      HTTPretty.POST,
+      FAKE_EXCHANGE_URL,
+      body=CREATE_FOLDER_RESPONSE.encode('utf-8'),
+      content_type='text/xml; charset=utf-8',
+    )
+
+    self.folder.display_name = TEST_FOLDER.display_name
+    self.folder.parent_id = TEST_FOLDER.parent_id
+    self.folder.create()
+
+    assert TEST_FOLDER.display_name in HTTPretty.last_request.body.decode('utf-8')
+
+  @httprettified
+  def test_can_set_parent_id(self):
+
+    HTTPretty.register_uri(
+      HTTPretty.POST,
+      FAKE_EXCHANGE_URL,
+      body=CREATE_FOLDER_RESPONSE.encode('utf-8'),
+      content_type='text/xml; charset=utf-8',
+    )
+
+    self.folder.display_name = TEST_FOLDER.display_name
+    self.folder.parent_id = TEST_FOLDER.parent_id
+    self.folder.create()
+
+    assert TEST_FOLDER.display_name in HTTPretty.last_request.body.decode('utf-8')
+
+  @httprettified
+  def test_can_set_folder_type(self):
+
+    HTTPretty.register_uri(
+      HTTPretty.POST,
+      FAKE_EXCHANGE_URL,
+      body=CREATE_FOLDER_RESPONSE.encode('utf-8'),
+      content_type='text/xml; charset=utf-8',
+    )
+
+    self.folder.display_name = TEST_FOLDER.display_name
+    self.folder.parent_id = TEST_FOLDER.parent_id
+    self.folder.folder_type = TEST_FOLDER.folder_type
+    self.folder.create()
+
+    assert TEST_FOLDER.folder_type in HTTPretty.last_request.body.decode('utf-8')
---
+++ tests/exchange2010/test_get_folder.py
@@ -0,0 +1,99 @@
+"""
+(c) 2013 LinkedIn Corp. All rights reserved.
+Licensed under the Apache License, Version 2.0 (the "License");?you may not use this file except in compliance with the License. You may obtain a copy of the License at  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software?distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+"""
+import httpretty
+from nose.tools import eq_, raises
+from pyexchange import Exchange2010Service
+from pyexchange.connection import ExchangeNTLMAuthConnection
+from pyexchange.exceptions import *
+
+from .fixtures import *
+
+
+class Test_ParseFolderResponseData(object):
+  folder = None
+
+  @classmethod
+  def setUpAll(cls):
+
+    @httpretty.activate  # this decorator doesn't play nice with @classmethod
+    def fake_folder_request():
+
+      service = Exchange2010Service(
+        connection=ExchangeNTLMAuthConnection(
+          url=FAKE_EXCHANGE_URL,
+          username=FAKE_EXCHANGE_USERNAME,
+          password=FAKE_EXCHANGE_PASSWORD,
+        )
+      )
+
+      httpretty.register_uri(
+        httpretty.POST,
+        FAKE_EXCHANGE_URL,
+        body=GET_FOLDER_RESPONSE.encode('utf-8'),
+        content_type='text/xml; charset=utf-8',
+      )
+
+      return service.folder().get_folder(id=TEST_FOLDER.id)
+
+    cls.folder = fake_folder_request()
+
+  def test_canary(self):
+    assert self.folder is not None
+
+  def test_folder_id_was_not_changed(self):
+    eq_(self.folder.id, TEST_FOLDER.id)
+
+  def test_folder_has_a_name(self):
+    eq_(self.folder.display_name, TEST_FOLDER.display_name)
+
+  def test_folder_has_a_parent(self):
+    eq_(self.folder.parent_id, TEST_FOLDER.parent_id)
+
+  def test_folder_type(self):
+    eq_(self.folder.folder_type, TEST_FOLDER.folder_type)
+
+
+class Test_FailingToGetFolders():
+
+  service = None
+
+  @classmethod
+  def setUpAll(cls):
+
+    cls.service = Exchange2010Service(
+      connection=ExchangeNTLMAuthConnection(
+        url=FAKE_EXCHANGE_URL,
+        username=FAKE_EXCHANGE_USERNAME,
+        password=FAKE_EXCHANGE_PASSWORD
+      )
+    )
+
+  @raises(ExchangeItemNotFoundException)
+  @httpretty.activate
+  def test_requesting_an_folder_id_that_doest_exist_throws_exception(self):
+
+    httpretty.register_uri(
+      httpretty.POST, FAKE_EXCHANGE_URL,
+      body=ITEM_DOES_NOT_EXIST.encode('utf-8'),
+      content_type='text/xml; charset=utf-8',
+    )
+
+    self.service.folder().get_folder(id=TEST_FOLDER.id)
+
+  @raises(FailedExchangeException)
+  @httpretty.activate
+  def test_requesting_an_folder_and_getting_a_500_response_throws_exception(self):
+
+    httpretty.register_uri(
+      httpretty.POST,
+      FAKE_EXCHANGE_URL,
+      body=u"",
+      status=500,
+      content_type='text/xml; charset=utf-8',
+    )
+
+    self.service.folder().get_folder(id=TEST_FOLDER.id)
