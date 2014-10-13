9460773f2e56b6b6ba1a68465d0c1deae53b03e7
Alejandro Ramirez <alejandro@cs.txstate.edu>
2014-10-12 19:02:45 -0500

    Flake8 fixes

--- tests/exchange2010/test_get_event.py
+++ tests/exchange2010/test_get_event.py
@@ -8,9 +8,10 @@ import httpretty
 from nose.tools import eq_, raises
 from pyexchange import Exchange2010Service
 from pyexchange.connection import ExchangeNTLMAuthConnection
-from pyexchange.exceptions import *
+from pyexchange.exceptions import *  # noqa
 
-from .fixtures import *
+from .fixtures import *  # noqa
+
 
 class Test_ParseEventResponseData(object):
   event = None
@@ -18,19 +19,24 @@ class Test_ParseEventResponseData(object):
   @classmethod
   def setUpAll(cls):
 
-    @httpretty.activate # this decorator doesn't play nice with @classmethod
+    @httpretty.activate  # this decorator doesn't play nice with @classmethod
     def fake_event_request():
 
-      service = Exchange2010Service(connection=ExchangeNTLMAuthConnection(url=FAKE_EXCHANGE_URL, username=FAKE_EXCHANGE_USERNAME, password=FAKE_EXCHANGE_PASSWORD))
+      service = Exchange2010Service(
+        connection=ExchangeNTLMAuthConnection(
+          url=FAKE_EXCHANGE_URL, username=FAKE_EXCHANGE_USERNAME, password=FAKE_EXCHANGE_PASSWORD
+        )
+      )
 
-      httpretty.register_uri(httpretty.POST, FAKE_EXCHANGE_URL,
-                                 body=GET_ITEM_RESPONSE.encode('utf-8'),
-                                 content_type='text/xml; charset=utf-8')
+      httpretty.register_uri(
+        httpretty.POST, FAKE_EXCHANGE_URL,
+        body=GET_ITEM_RESPONSE.encode('utf-8'),
+        content_type='text/xml; charset=utf-8',
+      )
 
       return service.calendar().get_event(id=TEST_EVENT.id)
 
     cls.event = fake_event_request()
-
 
   def test_canary(self):
     assert self.event is not None
@@ -99,33 +105,33 @@ class Test_FailingToGetEvents():
   @classmethod
   def setUpAll(cls):
 
-    cls.service = Exchange2010Service(connection=ExchangeNTLMAuthConnection(url=FAKE_EXCHANGE_URL,
-                                                                        username=FAKE_EXCHANGE_USERNAME,
-                                                                        password=FAKE_EXCHANGE_PASSWORD))
+    cls.service = Exchange2010Service(
+      connection=ExchangeNTLMAuthConnection(
+        url=FAKE_EXCHANGE_URL, username=FAKE_EXCHANGE_USERNAME, password=FAKE_EXCHANGE_PASSWORD
+      )
+    )
 
   @raises(ExchangeItemNotFoundException)
   @httpretty.activate
   def test_requesting_an_event_id_that_doest_exist_throws_exception(self):
 
-    httpretty.register_uri(httpretty.POST, FAKE_EXCHANGE_URL,
-                                 body=ITEM_DOES_NOT_EXIST.encode('utf-8'),
-                                 content_type='text/xml; charset=utf-8')
+    httpretty.register_uri(
+      httpretty.POST, FAKE_EXCHANGE_URL,
+      body=ITEM_DOES_NOT_EXIST.encode('utf-8'),
+      content_type='text/xml; charset=utf-8',
+    )
 
     self.service.calendar().get_event(id=TEST_EVENT.id)
-
 
   @raises(FailedExchangeException)
   @httpretty.activate
   def test_requesting_an_event_and_getting_a_500_response_throws_exception(self):
 
-    httpretty.register_uri(httpretty.POST, FAKE_EXCHANGE_URL,
-                                 body=u"",
-                                 status=500,
-                                 content_type='text/xml; charset=utf-8')
+    httpretty.register_uri(
+      httpretty.POST, FAKE_EXCHANGE_URL,
+      body=u"",
+      status=500,
+      content_type='text/xml; charset=utf-8',
+    )
 
     self.service.calendar().get_event(id=TEST_EVENT.id)
-
-
-
-
-
